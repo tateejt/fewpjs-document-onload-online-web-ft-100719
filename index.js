10c8fede8b03235a46921a51c690e8c97a04433c
Emery Tate <tateejt@gmail.com>
2020-02-01 01:34:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function(){
-  console.log("JavaScript is so cool. It lets me add text to my page programmatically.' to equal 'This is really cool!");
+  console.log("This is really cool!");
 });
 
 console.log(
